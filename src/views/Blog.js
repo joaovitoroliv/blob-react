af57ae5d2f7c7f08d5e27cc29fc1dc7de496c064
João Vitor Oliveira Souza <joaovitoroliveirasouza@MacBook-Air-de-Joao.local>
2025-04-02 00:26:08 -0300

    feat: finaliza hero

--- src/views/Blog.js
+++ src/views/Blog.js
@@ -1,7 +1,20 @@
+import styles from "./Blog.module.css"
+import { Link } from "react-router-dom"
+
 export function Blog() {
     return (
-        <h1>
-            Pagina Blog
-        </h1>
+        <>
+            <div className={styles.frontPageWrapper}>
+                <div className={styles.heroOverlay}>
+                    <img className={styles.hero}
+                        src="/herobanner.jpg"
+                        alt=""
+                    />
+                        <div className={styles.siteDescriptionWrapper}>
+                            <Link className={styles.button} to={"/sobre"}>Tecnologia, React e Frameworks - Confira as últimas postagens</Link>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
