dd68e8d148e299aafa387bd1171321c36238d150
João Vitor Oliveira Souza <joaovitoroliveirasouza@MacBook-Air-de-Joao.local>
2025-04-01 22:24:08 -0300

    feat: finaliza navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,35 @@
 import { Link } from "react-router-dom";
+import styles from "./Header.module.css"
+import { FaGithub } from "react-icons/fa";
 
 export function Header() {
     return (
         <>
-    <h1>
-        Meu blog
-    </h1>
-    <ul>
-        <li>
-            <Link to={"/"}>Home</Link>
-        </li>
-        <li>
-            <Link to={"/sobre"}>Sobre</Link>
-        </li>
+    <nav className={styles.navbar}>
+  <div className={styles.navbarLeft}>
+    <a href="/" className={styles.logo}>
+      React Blog
+    </a>
+  </div>
+  <div className={styles.navbarCenter}>
+    <ul className={styles.navLinks}>
+      <li>
+        <Link to={"/"}>Blog</Link>
+      </li>
+      <li>
+        <Link to={"/sobre"}>Sobre</Link>
+      </li>
+      <li>
+      < Link to={"/descricao"}>Descrição</Link>
+      </li>
     </ul>
+  </div>
+  <div className={styles.navbarRight}>
+    <a className={styles.githubLogo} href="https://github.com/joaovitoroliv" target="_blank" rel="noopener noreferrer">
+        <FaGithub size={36} />
+      </a>
+  </div>
+</nav>
     </>
     )
 }
